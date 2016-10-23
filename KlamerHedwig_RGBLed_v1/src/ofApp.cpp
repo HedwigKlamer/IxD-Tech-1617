5e88074cb642623913a0e11fd8b13be5358d501c
HedwigKlamer <hedwigklamer@hotmail.com>
2016-10-23 13:48:37 +0200

    RGB led
    Werkt nog steeds niet maar is wel netter gemaakt

--- KlamerHedwig_RGBLed_v1/src/ofApp.cpp
+++ KlamerHedwig_RGBLed_v1/src/ofApp.cpp
@@ -7,27 +7,30 @@ void ofApp::setup()
 {
 	ofAddListener(arduino.EInitialized, this, &ofApp::setupArduino);
 
-	arduino.connect("COM3");
-	arduino.sendFirmwareVersionRequest();
+	arduino.connect("COM3", 57600);
+	arduino.sendFirmwareVersionRequest();\
+	bSetupArduino = false;	// flag so we setup arduino when its ready
 }
 
 
 void ofApp::update() {
 	arduino.update();
 	
-	arduino.sendDigital(PIN_RED, 255);
-	arduino.sendDigital(PIN_GREEN, 255);
-	arduino.sendDigital(PIN_BLUE, 0);
+	//arduino.sendDigital(PIN_RED, 255);
+	//arduino.sendDigital(PIN_GREEN, 255);
+	//arduino.sendDigital(PIN_BLUE, 0);
 
 }
 
-void ofApp::draw()
-{
+void ofApp::draw(){
 	ofBackground(0);
 }
 
 void ofApp::setupArduino(const int& version) {
 	ofRemoveListener(arduino.EInitialized, this, &ofApp::setupArduino);
+
+	// it is now safe to send commands to the Arduino
+	bSetupArduino = true;
 
 	arduino.sendDigitalPinMode(PIN_RED, ARD_PWM);
 	arduino.sendDigitalPinMode(PIN_GREEN, ARD_PWM);
@@ -39,6 +42,15 @@ void ofApp::setupArduino(const int& version) {
 
 }
 
+void ofApp::updateArduino() {
+	// do not send anything until the arduino has been set up
+	if (bSetupArduino) {
+		arduino.sendPwm(PIN_RED, 255);
+		arduino.sendPwm(PIN_BLUE, 0);
+		arduino.sendPwm(PIN_GREEN,255);
+	}
+}
+
 void ofApp::digitalPinChanged(const int& pinNum) {
 	ofLogNotice() << "Digital Pin " << pinNum << " value: " << arduino.getDigital(pinNum) << endl;
 }
@@ -46,11 +58,5 @@ void ofApp::digitalPinChanged(const int& pinNum) {
 void ofApp::analogPinChanged(const int& pinNum) {
 	ofLogNotice() << "Analog Pin " << pinNum << " value: " << arduino.getAnalog(pinNum) << endl;
 
-}
-
-void ofApp::keyPressed(int key) {
-	if (key == ' ') {
-		arduino.disconnect();
-	}
 }
 
--- KlamerHedwig_RGBLed_v1/src/ofApp.h
+++ KlamerHedwig_RGBLed_v1/src/ofApp.h
@@ -12,14 +12,15 @@ public:
 	void setup();
 	void update();
 	void draw();
-	void keyPressed(int key);
 
+	bool		bSetupArduino;			// flag variable for setting up arduino once
 
 private:
 	ofArduino arduino;
 	void setupArduino(const int& version);
 	void digitalPinChanged(const int& pinNum);
 	void analogPinChanged(const int& pinNum);
+	void updateArduino();
 
 };
 
